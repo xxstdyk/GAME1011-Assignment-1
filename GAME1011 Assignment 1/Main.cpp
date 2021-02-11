c7b7cbd99d99c1bc696b43c6cfd09c9458d6a115
Cass <cass.a.didier@gmail.com>
2021-02-10 23:55:57 -0500

    Added Menus and Clear-Screen Function
    Added main menu with input validation to the main.cpp.
    Added clear screen and pause function to the Utility class

--- GAME1011 Assignment 1/Main.cpp
+++ GAME1011 Assignment 1/Main.cpp
@@ -8,6 +8,28 @@ std::vector<std::string> last_names;
 
 int main() {
 
+	int numParticipants; // put the number of participants in here
+
+	do {
+		std::cout << "Welcome." << std::endl;
+		std::cout << "How many participants are going to participate in the survey?" << std::endl;
+		std::cout << "Note: This program allows a maximum of 500 participants." << std::endl;
+		std::cin >> numParticipants;
+
+		if (numParticipants > 500 || numParticipants < 1) {   //Input Validation
+			std::cout << "ERROR: The number of participants has to be at least 1 and a maximum of 500." << std::endl;
+			Util::PauseClear();
+		}
+
+	} while (numParticipants > 500 || numParticipants < 1);  //Repeats while input is invalid
+
+
+	Util::ClearScreen();
+
+	for (int i = 0; i < numParticipants; ++i) {
+		// generate participants & add them to Survey class
+	}
+
 	std::ifstream istream;
 
 	// Reading in a list of first names
@@ -18,31 +40,50 @@ int main() {
 		while (getline(istream, name))
 			first_names.push_back(name);
 
-	} else
+	}
+	else
 		std::cerr << "Names file not found." << std::endl;
 
 	istream.close();
-	
+
 	// Reading in a list of last names
 	istream.open("last-names.txt");
 
-	if (istream.is_open()) { 
+	if (istream.is_open()) {
 		std::string lastName;
 		while (getline(istream, lastName))
 			last_names.push_back(lastName);
 
-	} else
+	}
+	else
 		std::cerr << "Last Names file not found." << std::endl;
 
 	istream.close();
 
-	// Cass do the menu here please thanks lov u : )
+	int userInput;
+	bool proceed = false;
 
-	int numParticipants; // put the number of participants in here
-	
-	for (int i = 0; i < numParticipants; ++i) {
-		
-	}
+	do {
+		std::cout << "Are you ready to process the servey?" << std::endl;
+		std::cout << "[1] Yes\t[2] Exit" << std::endl;
+
+		switch (userInput) {
+		case 1:
+			proceed = true;
+			// process function here
+			break;
+		case 2:
+			return 0;
+		default: // If the user enters an incorrect number, they can retry.
+			std::cout << "Please enter a valid option." << std::endl;
+			proceed = false;
+			Util::PauseClear();
+			break;
+		}
+
+	} while (proceed == false);
 
 	return 0;
+
+
 }
