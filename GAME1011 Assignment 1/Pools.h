3972a0a428340244acb6c57bc48e1c149df26241
Cass <cass.a.didier@gmail.com>
2021-02-11 01:08:40 -0500

    HardCoded Pool Elements
    Added list of colleges, Majors, and Gaming Platforms

--- GAME1011 Assignment 1/Pools.h
+++ GAME1011 Assignment 1/Pools.h
@@ -7,27 +7,102 @@
 namespace Pool {
 
 	// Hardcoded list of colleges
-	static const int NUM_COLLEGES = 1;
+	static const int NUM_COLLEGES = 7;
 	static std::string COLLEGES[NUM_COLLEGES] = {
-
+		"George Brown College",
+		"Seneca College",
+		"Centennial College",
+		"York University",
+		"Humber College",
+		"University of Toronto",
+		"Ryserson University",
 	};
 
 	// Hardcoded list of programs
-	static const int NUM_PROGRAMS = 1;
+	static const int NUM_PROGRAMS = 50;
 	static std::string PROGRAMS[NUM_PROGRAMS] = {
-
+		"Business",
+		"Game Programming",
+		"Linguistics",
+		"Computer Science",
+		"User Experience Design",
+		"Architecture",
+		"Applied Mathematics",
+		"Hygenics",
+		"English",
+		"French",
+		"Spanish",
+		"Mandarin",
+		"Japanese",
+		"East Asian Studies",
+		"Italian",
+		"South American Studies"
+		"Nursing",
+		"Forensics",
+		"Bio-Chemistry",
+		"Kinesiology",
+		"Film",
+		"Dance",
+		"History",
+		"Sociology",
+		"Psychology",
+		"Chemistry",
+		"Zoology",
+		"Game Design",
+		"Accounting",
+		"Graphic Design",
+		"Anthropology",
+		"Financial Economics",
+		"Drama",
+		"Environmental Science",
+		"Criminology",
+		"Data Science",
+		"Animation and 3D Modeling",
+		"ASL Translation",
+		"Jewelery Making",
+		"Interaction Design",
+		"Software Engineering",
+		"Fashion Technology",
+		"Sound Design and Production",
+		"Professional Writing",
+		"Construction and Engineering",
+		"Plumbing",
+		"Dental Hygiene",
+		"Fitness and Health",
+		"Neutrition",
+		"Office Administration",
+		"Education",
 	};
 
 	// Hardcoded list of streaming services
-	static const int NUM_STREAMING_SERVICES = 1;
+	static const int NUM_STREAMING_SERVICES = 11;
 	static std::string STREAMING_SERVICES[NUM_STREAMING_SERVICES] = {
-
+		"Netflix",
+		"Prime Video",
+		"Crave",
+		"HBO Max",
+		"Disney Plus",
+		"Hulu",
+		"Crackle",
+		"Apple TV",
+		"Criterion Channel",
+		"CBC Gem",
+		"CTV",
 	};
 
 	// Hardcoded list of gaming devices
-	static const int NUM_GAMING_DEVICES = 1;
+	static const int NUM_GAMING_DEVICES = 10;
 	static std::string GAMING_DEVICES[NUM_GAMING_DEVICES] = {
-
+		"Nintendo Switch",
+		"PC",
+		"Xbox",
+		"Play Station",
+		"Wii",
+		"Gameboy",
+		"Mobile",
+		"Nintendo 3DS",
+		"Atari",
+		"Tamogachi"
 	};
 }
 
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 ### Joint effort assignment by
 - Oostdyk, Nicholas - 101335730
+- Didier, Cassandra - 101128745
